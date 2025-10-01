cf42c840078b5ce7267e379f5e3b82dcbef8fcf3
marcodeum <marco.souza300322@gmail.com>
2025-09-30 22:09:19 -0300

    CRUD de Usuários

--- src/Pacotinho/Pessoa.java
+++ src/Pacotinho/Pessoa.java
@@ -1,4 +1,99 @@
 package Pacotinho;
 
+import java.util.Date;
+
 public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private Date dataNasc;
+    private int idade;
+    private int telefone;
+    private char sexo;
+    private String endereço;
+
+    public Pessoa() {
+        this.nome = "";
+        this.sobrenome = "";
+        this.dataNasc = new Date();
+        this.idade = 0;
+        this.telefone = 0;
+        this.sexo = ' ';
+        this.endereço = "";
+    }
+
+    public Pessoa(String nome, String sobrenome, Date dataNasc, int telefone, char sexo, String endereço) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNasc = dataNasc;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.endereço = endereço;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        Date currentDate = new Date();
+        this.idade = currentDate.getYear() - this.dataNasc.getYear();
+        return this.idade;
+    }
+
+    public String getFullSexo() {
+        if (this.sexo == 'F' || this.sexo == 'f'){
+            return "Femenino";
+        } else if (this.sexo == 'M' || this.sexo == 'm') {
+            return "Masculino";
+        }
+        return "👽";
+    }
+
+    public String info(){
+        return nome +" "+sobrenome+" | "+getIdade()+" anos | "+telefone+" | "+ getFullSexo() +"\n"+endereço;
+    }
 }
--- src/Pacotinho/Usuario.java
+++ src/Pacotinho/Usuario.java
@@ -1,4 +1,55 @@
 package Pacotinho;
 
-public class Usuario {
+import java.util.Date;
+
+public class Usuario extends Pessoa{
+    private String login;
+    private String senha;
+    private String email;
+
+    public Usuario(String nome, String sobrenome, Date dataNasc, int telefone, char sexo, String endereço, String login, String senha, String email) {
+        super(nome, sobrenome, dataNasc, telefone, sexo, endereço);
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public Usuario(){
+        super();
+        this.login = "";
+        this.senha = "";
+        this.email = "";
+    }
+    public Usuario(String login, String senha, String email) {
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public boolean isEqual(String senha){
+        return senha.equals(this.senha);
+    }
+    public String info(){
+        return "Dados do usuário: \n"+
+        super.info() + "\n" + this.email + "\n" + "Username: " + this.login;
+    }
 }
